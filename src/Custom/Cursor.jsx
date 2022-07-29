2208c042f5a23bbaba193e0a89d793682b2a008b
aibek shamshiev <aibek@MacBook-Air-aibek.local>
2022-07-29 15:41:31 +0600

    fix cursor bugs

--- src/Custom/Cursor.jsx
+++ src/Custom/Cursor.jsx
@@ -14,7 +14,7 @@ const Cursor = () => {
       cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px,0)`;
     });
   }, []);
-  return <div className='app-coursor' ref={cursorRef} />;
+  return <div className='app-cursor' ref={cursorRef} />;
 };
 
 export default Cursor;
