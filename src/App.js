b6c4e098a09200651a771ce16d2da98b70b93f8a
aibek shamshiev <aibek@MacBook-Air-aibek.local>
2022-07-29 14:40:41 +0600

    final project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { photos } from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <div className='logo'>billy</div>
+        <ul>
+          <li>works</li>
+          <li>about</li>
+        </ul>
+      </div>
+      <div className='top-banner'>
+        <div className='top-banner-content'>
+          <h1>independent designer</h1> based in{' '}
+          <span className='denmark'>Denmark</span>
+          <iframe
+            title={'src'}
+            className={`resp-iframe `}
+            src={
+              'https://player.vimeo.com/video/91284753?autoplay=1&loop=1&autopause=0&muted=1'
+            }
+            width='640'
+            height='360'
+            frameborder='0'
+            webkitallowfullscreen
+            mozallowfullscreen
+            allowfullscreen
+            allow='autoplay; fullscreen'
+            controls='0'
+          ></iframe>
+          <div className='top-banner-scrolldown'>
+            <span>☟</span>
+          </div>
+        </div>
+      </div>
+      <div className='main-container menu-list'>
+        {photos.map(({ title, url }, index) => (
+          <div key={url} className={'menu-item'}>
+            <h1 className='menu-title'>
+              <span className='counter'>{index + 1}</span>
+              {title}
+            </h1>
+            <h1 className='menu-title clone'>
+              <span className='counter'>{index + 1}</span>
+              {title}
+            </h1>
+            <div className='menu-image-container'>
+              <div
+                className='menu-image'
+                style={{ backgroundImage: `url(${url})` }}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const photos = [
+  {
+    title: 'Nike workout',
+    url: 'https://images.unsplash.com/photo-1509983165097-0c31a863e3f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=100',
+  },
+  {
+    title: 'Virtual Reality',
+    url: 'https://images.unsplash.com/photo-1588590560438-5e27fe3f6b71?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=3372&q=100',
+  },
+  {
+    title: 'Vans skaters',
+    url: 'https://images.unsplash.com/photo-1612100389024-106c3cfdb7d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3331&q=100',
+  },
+  {
+    title: 'Lego builders',
+    url: 'https://images.unsplash.com/photo-1572790082878-f254f61214fa?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=3289&q=100',
+  },
+  {
+    title: 'Adidas kicks',
+    url: 'https://images.unsplash.com/photo-1528858755052-cacfa0dd164a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2229&q=100',
+  },
+];
+
+export { photos };
